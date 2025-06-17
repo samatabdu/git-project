2518d64a6331e489c97e9294ab33f378facc6a17
aabdyldaev <aabdyldaev@srs.lan>
2025-06-17 15:39:31 +0600

    change index.php file

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
+<h1>HTML5 &lt;dialog&gt; Element</h1>
+<p id="result">Click on the button to open modal</p>
+<button id="openButton">Open Modal</button>
 
-echo "My first commit";
+<!-- element -->
+<dialog id="confirmationDialog">
+  <!-- content -->
+  <button aria-label="Close modal" id="buttonCloseX">✖</button>
+  
+  <h1>Modal message here</h1>
+  <p>Are you sure to confirm this action?</p>
+  
+  <input type="text" name="title"/>
+  <input type="text" name="description"/>
+  
+  <button id="buttonOk">Confirm</button>
+  <button id="buttonClose">Close</button>
+</dialog>
