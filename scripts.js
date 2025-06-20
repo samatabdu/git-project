16bf2dd7d67928765b6cb900e610b3ea15dc5cf3
aabdyldaev <aabdyldaev@srs.lan>
2025-06-20 11:16:56 +0600

    Add side bar

--- scripts.js
+++ scripts.js
@@ -45,3 +45,26 @@ buttonCloseX.addEventListener("click", () => {
 confirmationDialog.addEventListener("cancel", (event) => {
   result.textContent = "Result: <dialog> was canceled by ESC key press (cancel event)";
 });
+
+//elements 
+const btnToggler = window.document.querySelector(".navbar-toggler"); 
+const inputSearch = window.document.querySelector(".navbar-search"); 
+const iconSearch = window.document.querySelector("#icon-search");
+const navbar = window.document.querySelector(".navbar");
+
+//events
+btnToggler.addEventListener('click', () => {
+    navbar.classList.toggle('active'); 
+});
+
+inputSearch.addEventListener('click', () => {
+    if(!navbar.classList.contains("active")) {
+        navbar.classList.add('active'); 
+    }
+});
+
+iconSearch.addEventListener('click', () => {
+    if(!navbar.classList.contains("active")) {
+        navbar.classList.add('active'); 
+    }
+});
