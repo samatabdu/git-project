7e233166e853f8d0bc7d34768f8d8b66fbdee6ee
aabdyldaev <aabdyldaev@srs.lan>
2025-06-26 17:51:41 +0600

    add code snippet

--- scripts.js
+++ scripts.js
@@ -68,3 +68,26 @@ iconSearch.addEventListener('click', () => {
         navbar.classList.add('active'); 
     }
 });
+
+
+(function(){
+  
+  document.addEventListener('DOMContentLoaded', function(event) {
+
+    var list = document.querySelectorAll('.make-snippet');
+
+    [].forEach.call(list, function(el) {
+      var snippet = el.innerHTML.replace(/</g,'&lt;');
+          snippet = snippet.replace(/ /g,'&nbsp;');
+      var code = '<pre class="language-markup"><code>'+snippet+'</pre></code>';
+      el.insertAdjacentHTML('afterend',code);
+    });
+    
+    // if your page has prism.js you get syntax highlighting
+    if(window.Prism){
+      Prism.highlightAll(false);
+    }
+
+  });
+
+})();
